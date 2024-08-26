b7d71c3870e82f4eb6af0be7f666583f5c51888e
Navaneeth Rameshwaram <rnavaneethkishore@gmail.com>
2024-08-26 12:49:28 -0700

    updated config

--- src/utils/authenticatePostgres.js
+++ src/utils/authenticatePostgres.js
@@ -10,10 +10,7 @@ const sequelize = new Sequelize('', process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: {
-      require: true,
-      ca: fs.readFileSync(`${__dirname}/us-west-1-bundle.pem`),
-    },
+    ssl: false,
   }
 });
 
